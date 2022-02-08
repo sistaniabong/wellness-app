0a5512c0a244d60bcc7a0989cbe024ec6d5fb541
Jing Li <lijing900913@gmail.com>
2022-02-07 20:31:44 -0500

    Modify-App

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer class="p-1 teal lighten-2 display-flex align-center">
+            <div class="flex-column justify-center align-center text-center">
+                <h3 class="m-0" >&copy; 2022 - Girl Gode Group </h3>
+            </div>
+        </footer>
+    );
+  };
+  
+  export default Footer;
