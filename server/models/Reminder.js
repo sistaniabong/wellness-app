300fdc7f3a4feb24f49f356815f5dd5924e1a896
Sistania Bong <sistaniameilina@gmail.com>
2022-02-08 08:25:10 +0700

    models and add bcrypt

---
+++ server/models/Reminder.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const reminderSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    time_interval:{
+        type: Number,
+        default: 0,
+    },
+    complete_count: {
+        type: Number,
+        default: 0,
+    },
+    skip_count: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    activity: {
+        type: Schema.Types.ObjectId,
+        ref: 'Activity'
+    }
+});
+
+const Reminder = model('Reminder', reminderSchema);
+
+module.exports = Reminder;
