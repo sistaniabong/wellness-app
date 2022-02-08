5f4f5510d85199e7281c10363abcbad851e2a493
kperkins97 <perkins.kelsey@yahoo.com>
2022-02-08 13:31:28 -0600

    fixing queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,29 +1,38 @@
 import { gql } from '@apollo/client';
 
-export const GET_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
+export const GET_USERS = gql`
+  query getUsers {
+    users {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const GET_SINGLE_USER = gql`
+  query getSingleUser($userId: ID!) {
+    user (userId: $userId)
       _id
       username
       email
       activities {
         _id
-        name
-        reminders
+        title
+        duration
       }
     }
   }
-`
+`;
+
 
 export const GET_REMINDERS = gql`
-  query getReminders($reminderId: ID!) {
-    reminders (reminderId: $reminderId) {
+  query getReminders {
+    reminders {
       _id
-      name
-      users {
-        _id
-        username
-      }
+      title
+      time_interval
+      createdAt
     }
   }
 `;
@@ -32,10 +41,13 @@ export const GET_SINGLE_REMINDER = gql`
   query getSingleReminder($reminderId: ID!) {
     reminder (reminderId: $reminderId) {
       _id
-      name
-      users {
+      title
+      time_interval
+      createdAt
+      activites {
         _id
-        username
+        title
+        duration
       }
     }
   }
@@ -46,11 +58,9 @@ export const GET_ACTIVITIES = gql`
   query getActivities {
     activites {
       _id
-      name
-      users {
-        _id
-        username
-      }
+      title
+      duration
+      createdAt
     }
   }
 `;
@@ -59,17 +69,44 @@ export const GET_SINGLE_ACTIVITY = gql`
   query getSingleActivity($activityId: ID!) {
     activity (activityId: $activityId) {
       _id
-      name
-      users {
+      title
+      duration
+      user {
+        _id
         username
-        // will this arrary return properly?
-       reminders
+      } 
+      comments {
+        _id
+        commentText
+        createdAt
+        reminders {
+          _id
+          title
+          time_interval
+          todos {
+            name
+            status
+          }  
+        }
       }
     }
   }
 `;
 
-// may not need - double check with team
+
+export const GET_TODOS = gql`
+
+`;
+
+export const GET_SINGLE_TODO = gql`
+
+`;
+
+
+
+
+
+// Will come back for future developments: -KP
 
 // export const GET_MOOD = gql`
 //   query getMood {
