3196bed65ed31b55186c22acacb2b27ffd6a1446
Reagin Turner <rturnerafteragnes@gmail.com>
2022-02-07 20:31:05 -0500

    Working on mutations for the login and signup in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,19 +1,46 @@
 const { AuthenticationError } = require('apollo-server-express');
 // const { User, Activity, Mood, Reminder } = require('./models)
-// require auth token
 // const { signToken } = require('../utils/auth');
 // const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc'); // look at stripe documentation
 
 // Query and mutation, export to use in client
-module.exports = {
+const resolvers = {
     Query: {
         users: () => {
             return await User.find();
         }
     },
+    // query users activities
+    // query users moods
+    // query users reminders
+
 
     // add the authentication 
     Mutation: {
-        createUser
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if ( user) {
+              throw new AuthenticationError('No user with this email found!');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect password!');
+            }
+      
+            const token = signToken (user);
+            return { token, user };
+          },
+        signup: asynce (pare)
     }
 }
+
+module.exports = resolvers;
