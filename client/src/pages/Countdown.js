30eb582362db13d8adbd0651fca0590568cc3c86
Jing Li <lijing900913@gmail.com>
2022-02-12 14:17:18 -0500

    Countdown Outline

--- client/src/pages/Countdown.js
+++ client/src/pages/Countdown.js
@@ -1,63 +1,22 @@
-import React, { useEffect, useRef, useState }from 'react';
+import React, { useState }from 'react';
 import { useMutation } from '@apollo/client';
+import Timer from '../components/Countdown.js/Timer';
+import ToDoList from '../components/ToDoList/ToDoList';
 
 const Countdown = () =>{
-    const [timerHours, setTimerHours] = useState('00');
-    const [timerMinutes, setTimerMinutes] =useState('00');
-    const [timerSeconds, setTimerSeconds] =useState('00');
-    
-    let interval = useRef();
-    // find a method to transfer the duration and time_interval to milliseconds (duration*60*1000)
-    const startTimer = () => {
-        
-            // distance is the duration of the activity
-            // set duration=125.5 (2h5min30s) for example
-            const distance = 125.5;
-            const hours = Math.floor(distance/60); //2
-            const minutes = Math.floor(distance%60);// distance%60 == 5.5 floor is 5
-            const seconds = Math.floor(distance%60%minutes*60);// 5.5/5*60=30s
-
-            if (distance < 0){
-                // stop timer
-                clearInterval(interval);
-            }else{
-                // update timer
-                setTimerHours(hours);
-                setTimerMinutes(minutes);
-                setTimerSeconds(seconds);
-            }
-       
-    };
-
-    useEffect(() => {
-        const interval = setInterval(startTimer, 1000);
-        return () => clearInterval(interval)
-     })
-    // useEffect(() => {
-    //     startTimer();
-    //     return() => {
-    //         clearInterval(interval);
-    //     }
-    // })
-
-    return(
+    return (
         <main>
-            <h1>🕰 Countdown Timer</h1>
-            <div 
-                className="d-flex justify-content-around p-3 m-3"
-                style={{borderStyle: "double", borderRadius: "10px" }}
-            >
-                <span type="hour">{timerHours}</span>
-                <span>:</span>
-                <span type="minute">{timerMinutes}</span>
-                <span>:</span>
-                <span type="second">{timerSeconds}</span>
-            </div>
+            {/* CountdownTimer */}
+            <Timer />
+
+            {/* Todo List */}
+            <ToDoList />
             
-            
+            {/* Reminder Popup thing  */}
+
         </main>
         
     )
-};
+}
 
 export default Countdown;
