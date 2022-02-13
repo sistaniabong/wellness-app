2e459b23bed4138beb5af1e60dfbaa7e398f4b40
Jing Li <lijing900913@gmail.com>
2022-02-13 11:25:08 -0500

    Update ToDosForActivity.js

--- client/src/components/Countdown.js/ToDosForActivity.js
+++ client/src/components/Countdown.js/ToDosForActivity.js
@@ -11,7 +11,7 @@ const ToDosForActivity = ({todos}) =>{
             // }
 
             // test
-            
+
     }
 
     return (
@@ -43,7 +43,7 @@ const ToDosForActivity = ({todos}) =>{
                                         className='col-2 card-action' 
                                         style={{borderRadius: '10px'}}
                                         status={todo.status}
-                                        onClick={changeStatusHandler}
+                                        onClick={changeStatusHandler(todo)}
                                     >
                                         ✔️
                                     </button>
