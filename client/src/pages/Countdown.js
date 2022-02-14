0eeb1d1a8e72ff10a2063a624707bf26ba1075a7
Jing Li <lijing900913@gmail.com>
2022-02-14 15:24:31 -0500

    Count-down-clock-worked

--- client/src/pages/Countdown.js
+++ client/src/pages/Countdown.js
@@ -3,33 +3,21 @@ import { useQuery } from '@apollo/client';
 import { useMutation } from '@apollo/client';
 import Timer from '../components/Countdown/Timer';
 import ToDosForActivity from '../components/Countdown/ToDosForActivity';
-import { GET_SINGLE_ACTIVITY } from '../utils/queries';
+// import { GET_SINGLE_ACTIVITY } from '../utils/queries';
 
 const Countdown = () =>{
     // grab the activityId here. and pass to the component below
     const activityId = "62090c30b9a9ef3fddb08f75";
-
-    const { loading, data } = useQuery(GET_SINGLE_ACTIVITY,
-        {
-          variables: { activityId: activityId },
-        }
-        
-    );
-      console.log(data);
-      const duration = data?.duration || [];
-      console.log('duration:')
-      console.log(duration)
-      const todos = data?.todos || [];
-      console.log('todos:')
-      console.log(todos);
+    const duration = 1;
+      
 
     return (
         <main>
             {/* CountdownTimer */}
-                <Timer duration={duration}/>
+                <Timer duration={duration} activityId={activityId}/>
                 
-            {/* Todo List */}
-                <ToDosForActivity todos={todos}/>
+            {/* Todo List todos={todos} */}
+                {/* <ToDosForActivity /> */}
 
             {/* Reminder Popup thing */}
                 {/* Add a visible and invisible attribtue for the reminder card*/}
